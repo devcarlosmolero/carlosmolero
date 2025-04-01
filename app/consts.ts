63ec24b2f0739176f178e35b1a7603438a821ee7
devcarlosmolero <dev@carlosmolero.com>
2025-04-01 18:19:48 +0200

    update: meta title

--- app/consts.ts
+++ app/consts.ts
@@ -1,7 +1,7 @@
 export const IMAGE_KIT_BASE_URL = 'https://ik.imagekit.io/bvbysjq1r'
 
 export const SITE_STATIC_PATHS = ['']
-export const SITE_TITLE = 'Product Designer and Software Developer'
+export const SITE_TITLE = 'Product Design | UX Engineering | SWE'
 export const SITE_DESCRIPTION = `Product Designer and Software Developer. Strategizing & crafting user-friendly products with technical skill & design expertise.`
 export const SITE_NAME = 'Carlos Molero'
 export const SITE_BASE_URL = 'https://carlosmolero.com'
