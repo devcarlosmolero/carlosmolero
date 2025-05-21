0f1eb0d4840a9c13cd1f0ec064648fd52027ebea
devcarlosmolero <dev@carlosmolero.com>
2025-05-21 11:20:53 +0200

    update: portfolio

--- app/components/pages/Home/HomePortfolio.tsx
+++ app/components/pages/Home/HomePortfolio.tsx
@@ -1,41 +1,176 @@
-import { MoveUpRight } from 'lucide-react'
-import ImageKitImage from '~/components/atoms/ImageKitImage'
-import Card from '~/components/templates/Card'
+import { BriefcaseBusiness, Code, Layers, Link } from 'lucide-react'
+import { act, Fragment, useState } from 'react'
+import Button from '~/components/atoms/Button'
+import { cn } from '~/lib/utils'
 
 export default function HomePortfolio() {
+    const [activeTab, setActiveTab] = useState('notoriousClients')
+
     return (
         <section id="portfolio" className="space-y-8 rounded-md">
-            <h2 className="heading-gradient py-1 text-center text-4xl md:text-start md:text-5xl">
-                Works
-            </h2>
+            <div className="space-y-5">
+                <h2 className="heading-gradient py-1 text-center text-4xl md:text-start md:text-5xl">
+                    Works
+                </h2>
+                <div className="flex flex-wrap items-center justify-center gap-3 md:justify-start">
+                    <button
+                        onClick={() => setActiveTab('notoriousClients')}
+                        className={cn(
+                            'flex w-fit items-center gap-2 rounded-full border border-border-main px-4 py-2 text-sm transition-all duration-300 hover:border-text-two hover:text-text-two',
+                            activeTab === 'notoriousClients' &&
+                                'border-text-two text-text-two'
+                        )}
+                    >
+                        <BriefcaseBusiness className="size-4" />
+                        Notorious Clients
+                    </button>
+                    <button
+                        onClick={() => setActiveTab('concepts')}
+                        className={cn(
+                            'flex w-fit items-center gap-2 rounded-full border border-border-main px-4 py-2 text-sm transition-all duration-300 hover:border-text-two hover:text-text-two',
+                            activeTab === 'concepts' &&
+                                'border-text-two text-text-two'
+                        )}
+                    >
+                        <Layers className="size-4" />
+                        Concepts
+                    </button>
+                    <button
+                        onClick={() => setActiveTab('code')}
+                        className={cn(
+                            'flex w-fit items-center gap-2 rounded-full border border-border-main px-4 py-2 text-sm transition-all duration-300 hover:border-text-two hover:text-text-two',
+                            activeTab === 'code' &&
+                                'border-text-two text-text-two'
+                        )}
+                    >
+                        <Code className="size-4" />
+                        Code
+                    </button>
+                </div>
+            </div>
             <div className="grid gap-5 md:grid-cols-2">
-                <Card className="aspect-square group">
-                    <div className="overflow-hidden rounded-md">
-                        {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-                        <ImageKitImage
-                            src="/tr:f-webp,w-600,h-600/battlefront-2-campaign-ui-redesign.png"
-                            alt="Star Wars Battlefront II: Campaign UI Redesign"
-                            className="h-full w-full rounded-md transition-all duration-200 group-hover:scale-105"
+                {activeTab === 'notoriousClients' && (
+                    <Fragment>
+                        <NotoriousClientCard
+                            name="WizHelp"
+                            description="Wizhelp offers IT support services to businesses,
+                    aiming to boost productivity by managing systems,
+                    applications, and devices. They provide support
+                    across various environments, handle projects and
+                    implementations, and automate repetitive tasks."
+                            logoSrc="./wizhelp-logo.png"
+                            overlayBackgroundColor="hsla(264, 100%, 52%, 0.08)"
+                            landingHref="https://wizhelp.net"
                         />
-                    </div>
-                    <div className="mt-8 space-y-3">
-                        <div className="group flex cursor-pointer">
-                            <div className="w-full">
-                                <h4 className="text-3xl text-text-two">
-                                    Battlefront II: Campaign UI Redesign
-                                </h4>
-                            </div>
-                            <div>
-                                <MoveUpRight className="size-6 text-text-three transition-all duration-200 group-hover:rotate-45 group-hover:text-text-two" />
-                            </div>
-                        </div>
-                        <p className="text-lg text-text-one">
-                            A quick UI redesign of the Star Wars Battlefront II
-                            campaign menu.
+                        <NotoriousClientCard
+                            name="Beagly"
+                            description="Beag.ly is a platform that offers online surveys and rewards participants with gift cards of $5 or more upon completion. 
+                    Users can choose from various popular retailers like Amazon, Dunkin', CVS Pharmacy, Target, Whole Foods, and Walmart."
+                            logoSrc="./beagly-logo.png"
+                            landingHref="https://beag.ly"
+                        />
+                        <NotoriousClientCard
+                            name="LathosClub"
+                            description="Lathos Club is an exclusive ecosystem dedicated to developing, 
+                    connecting, and empowering bright minds. It offers members access to 
+                    expert knowledge, personalized mentorship, and a supportive community 
+                    to foster their entrepreneurial and personal growth."
+                            logoSrc="./lathosclub-logo.png"
+                            backgroundColor="bg-black/80"
+                            landingHref="https://lathosclub.com"
+                        />
+                        <NotoriousClientCard
+                            name="Peakz"
+                            description="On Peakz athletes get rewarded for what they do: you
+                    video yourself training, preparing and competing and
+                    post that content exclusively on your Peakz channel.
+                    Your fans become scouts and pay to see your content."
+                            logoSrc="./peakz-logo.png"
+                            overlayBackgroundColor="hsla(34, 87%, 60%, 0.08)"
+                            landingHref="https://app.peakz.com"
+                        />
+                        <NotoriousClientCard
+                            name="Voicit"
+                            description="Voicit is an AI-powered tool that provides online ChatGPT summaries for meeting notes. 
+                    It enables users to summarize and create customizable meeting minutes, 
+                    interacting with transcriptions, recordings, and intelligent chats to extract 
+                    key information from both online and in-person meetings. "
+                            logoSrc="./voicit-logo.png"
+                            landingHref="https://voicit.es"
+                        />
+                    </Fragment>
+                )}
+                {activeTab === 'concepts' && (
+                    <Fragment>
+                        <p>
+                            There are no concepts yet! Give me some time to
+                            upload some ;).
                         </p>
-                    </div>
-                </Card>
+                    </Fragment>
+                )}
+                {activeTab === 'code' && (
+                    <Fragment>
+                        <p>
+                            The repos are in Github but I still need to create
+                            this component ;).
+                        </p>
+                    </Fragment>
+                )}
             </div>
         </section>
+    )
+}
+
+function NotoriousClientCard({
+    backgroundColor = 'bg-white/80',
+    overlayBackgroundColor,
+    logoSrc,
+    landingHref,
+    name,
+    description,
+}: {
+    backgroundColor?: string
+    overlayBackgroundColor?: string
+    logoSrc: string
+    landingHref: string
+    name: string
+    description: string
+}) {
+    return (
+        <div className="flex flex-col items-start gap-5 rounded-md border border-border-main p-5">
+            <div className="flex h-full flex-col gap-3">
+                <div
+                    className={`h-[50px] min-w-[50px] max-w-[50px] rounded-md ${backgroundColor}`}
+                >
+                    <div
+                        style={{ backgroundColor: overlayBackgroundColor }}
+                        className="flex h-full w-full items-center justify-center"
+                    >
+                        <img alt={name} className="w-[25px]" src={logoSrc} />
+                    </div>
+                </div>
+                <div className="space-y-1">
+                    <h3 className="text-2xl text-text-two">{name}</h3>
+                    <p className="text-text-on-primary">{description}</p>
+                </div>
+            </div>
+            <div className="w-full rounded-md bg-base-secondary px-4 py-3">
+                <Button
+                    asLink
+                    to={landingHref}
+                    props={
+                        {
+                            target: '_blank',
+                        } as React.LinkHTMLAttributes<HTMLLinkElement>
+                    }
+                    variant="ghost"
+                    className="!gap-2 !px-0 !py-0"
+                    hasIcon
+                >
+                    <Link className="size-4 text-text-one" />
+                    Landing
+                </Button>
+            </div>
+        </div>
     )
 }
--- app/utils/services.tsx
+++
@@ -1,8 +0,0 @@
-import { renderToStaticMarkup } from 'react-dom/server'
-
-export function injectServiceBenefits(content: string) {
-    const regex = /--benefits--\n(\[.*?\])\n--benefits--/gs
-    return content.replace(regex, (_, jsonString) => {
-        return renderToStaticMarkup(<></>)
-    })
-}
