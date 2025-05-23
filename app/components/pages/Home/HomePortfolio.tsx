3e7c49bc61ea76cc6b9891828605b5ad8aed03c0
devcarlosmolero <dev@carlosmolero.com>
2025-05-23 19:20:31 +0200

    add: portfolio tabs animation

--- app/components/pages/Home/HomePortfolio.tsx
+++ app/components/pages/Home/HomePortfolio.tsx
@@ -64,73 +64,88 @@ export default function HomePortfolio({
                     </button>
                 </div>
             </div>
-            <div className="grid gap-5 md:grid-cols-2">
-                {activeTab === 'notoriousClients' && (
-                    <Fragment>
-                        {notoriousClientsData.map((client, index) => (
-                            <NotoriousClientCard
-                                key={`client-${index}`}
-                                name={client.name}
-                                description={client.description}
-                                logoSrc={client.logoSrc}
-                                overlayBackgroundColor={
-                                    client.overlayBackgroundColor
-                                }
-                                landingHref={client.landingHref}
-                                backgroundColor={client.backgroundColor}
-                            />
-                        ))}
-                    </Fragment>
-                )}
-                {activeTab === 'concepts' && (
-                    <Fragment>
-                        {conceptsData.map((concept, index) => (
-                            <Card
-                                key={`concept-${index}`}
-                                className="aspect-square group"
-                            >
-                                <div className="overflow-hidden rounded-md">
-                                    <video
-                                        loop
-                                        autoPlay
-                                        muted
-                                        playsInline
-                                        src={concept.videoSrc}
-                                        className="h-full w-full rounded-md transition-all duration-200 group-hover:scale-105"
-                                    />
-                                </div>
-                                <div className="mt-8 space-y-3">
-                                    <div className="group flex cursor-pointer">
-                                        <div className="w-full">
-                                            <h4 className="text-3xl text-text-two">
-                                                {concept.title}
-                                            </h4>
-                                        </div>
-                                        <div>
-                                            <MoveUpRight className="size-6 text-text-three transition-all duration-200 group-hover:rotate-45 group-hover:text-text-two" />
-                                        </div>
+            <div className="grid grid-cols-1 grid-rows-1 overflow-hidden">
+                <div
+                    className={cn(
+                        'col-span-1 col-start-1 row-span-1 row-start-1 grid gap-5 transition-all duration-300 md:grid-cols-2',
+                        activeTab === 'notoriousClients'
+                            ? 'translate-x-0 opacity-100'
+                            : 'h-0 translate-x-8 overflow-hidden opacity-0'
+                    )}
+                >
+                    {notoriousClientsData.map((client, index) => (
+                        <NotoriousClientCard
+                            key={`client-${index}`}
+                            name={client.name}
+                            description={client.description}
+                            logoSrc={client.logoSrc}
+                            overlayBackgroundColor={
+                                client.overlayBackgroundColor
+                            }
+                            landingHref={client.landingHref}
+                            backgroundColor={client.backgroundColor}
+                        />
+                    ))}
+                </div>
+                <div
+                    className={cn(
+                        'col-span-1 col-start-1 row-span-1 row-start-1 grid gap-5 transition-all duration-300 md:grid-cols-2',
+                        activeTab === 'concepts'
+                            ? 'translate-x-0 opacity-100'
+                            : 'h-0 translate-x-8 overflow-hidden opacity-0'
+                    )}
+                >
+                    {conceptsData.map((concept, index) => (
+                        <Card
+                            key={`concept-${index}`}
+                            className="aspect-square group"
+                        >
+                            <div className="overflow-hidden rounded-md">
+                                <video
+                                    loop
+                                    autoPlay
+                                    muted
+                                    playsInline
+                                    src={concept.videoSrc}
+                                    className="h-full w-full rounded-md transition-all duration-200 group-hover:scale-105"
+                                />
+                            </div>
+                            <div className="mt-8 space-y-3">
+                                <div className="group flex cursor-pointer">
+                                    <div className="w-full">
+                                        <h4 className="text-2xl text-text-two">
+                                            {concept.title}
+                                        </h4>
                                     </div>
-                                    <p className="text-lg text-text-one">
-                                        {concept.description}
-                                    </p>
+                                    <div>
+                                        <MoveUpRight className="size-6 text-text-three transition-all duration-200 group-hover:rotate-45 group-hover:text-text-two" />
+                                    </div>
                                 </div>
-                            </Card>
-                        ))}
-                    </Fragment>
-                )}
-                {activeTab === 'code' && (
-                    <Fragment>
-                        {repositoriesData.map((repo, index) => (
-                            <RepositoryCard
-                                key={`repo-${index}`}
-                                name={repo.name}
-                                description={repo.description}
-                                repoHref={repo.repoHref}
-                                language={repo.language}
-                            />
-                        ))}
-                    </Fragment>
-                )}
+                                <p className="text-text-one">
+                                    {concept.description}
+                                </p>
+                            </div>
+                        </Card>
+                    ))}
+                </div>
+                <div
+                    className={cn(
+                        'col-span-1 col-start-1 row-span-1 row-start-1 grid gap-5 transition-all duration-300 md:grid-cols-2',
+                        activeTab === 'code'
+                            ? 'translate-x-0 opacity-100'
+                            : 'h-0 translate-x-8 overflow-hidden opacity-0'
+                    )}
+                >
+                    {repositoriesData.map((repo, index) => (
+                        <RepositoryCard
+                            key={`repo-${index}`}
+                            name={repo.name}
+                            description={repo.description}
+                            repoHref={repo.repoHref}
+                            language={repo.language}
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     )
