5ea96aa78fa43ee1c8919425ab0422d880ec7b13
devcarlosmolero <dev@carlosmolero.com>
2025-06-23 16:12:27 +0200

    update: threads > mastodon

--- app/components/organisms/Footer.tsx
+++ app/components/organisms/Footer.tsx
@@ -1,4 +1,4 @@
-import { Github, Threads } from 'react-bootstrap-icons'
+import { Github, Mastodon, Threads } from 'react-bootstrap-icons'
 import Container from '../templates/Container'
 import { Link } from '@remix-run/react'
 import { Linkedin } from 'lucide-react'
@@ -8,14 +8,6 @@ export default function Footer() {
         <footer className="mx-5 mt-16 border border-b-0 border-l-0 border-r-0 border-border-main">
             <Container className="flex items-center justify-center px-4 py-8 md:justify-start">
                 <div className="flex items-center gap-x-3">
-                    <Link
-                        to="https://threads.net/@iscarlosmolero"
-                        className="cursor-pointer rounded-full border border-input-focus-border p-2 text-text-three hover:bg-base-tertiary hover:text-text-on-tertiary"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <Threads className="size-4" />
-                    </Link>
                     <Link
                         className="cursor-pointer rounded-full border border-input-focus-border p-2 text-text-three hover:bg-base-tertiary hover:text-text-on-tertiary"
                         to="https://linkedin.com/in/iscarlosmolero"
@@ -31,6 +23,21 @@ export default function Footer() {
                         rel="noreferrer"
                     >
                         <Github className="size-4" />
+                    </Link>
+                    <Link
+                        className="cursor-pointer rounded-full border border-input-focus-border p-2 text-text-three hover:bg-base-tertiary hover:text-text-on-tertiary"
+                        to="https://github.com/devcarlosmolero"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Github className="size-4" />
+                    </Link>
+                    <Link
+                        className="cursor-pointer rounded-full border border-input-focus-border p-2 text-text-three hover:bg-base-tertiary hover:text-text-on-tertiary"
+                        rel="me"
+                        to="https://mastodon.social/@iscarlosmolero"
+                    >
+                        <Mastodon className="size-4" />
                     </Link>
                 </div>
             </Container>
